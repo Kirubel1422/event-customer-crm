2e366e39b8b00ea569f0d10df6d417e8f55292a8
Kirubel1422 <kirubel739@gmail.com>
2025-05-31 04:09:21 -0400

    ref: updates error response format as well as adds new exception (ConstrainViolationException)

--- src/main/java/com/organization/crm/utils/GlobalExceptionHandler.java
+++ src/main/java/com/organization/crm/utils/GlobalExceptionHandler.java
@@ -1,15 +1,20 @@
 package com.organization.crm.utils;
 
+import jakarta.validation.ConstraintViolationException;
 import org.apache.coyote.BadRequestException;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpServerErrorException.InternalServerError;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,56 +22,88 @@ import java.util.Map;
 public class GlobalExceptionHandler {
     // Related to credentials
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<Map<String, String>> handleBadCredentialsException(
-      BadCredentialsException e
+    public ResponseEntity<Map<String, Object>> handleBadCredentialsException(
+      BadCredentialsException e,
+      WebRequest request
     ){
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(createErrorMap(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+                .body(createErrorMap(request.getDescription(false), e.getMessage()));
     }
 
     // Overall BadRequest
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<Map<String, String>> handleBadRequestException(
-            BadRequestException e
+    public ResponseEntity<Map<String, Object>> handleBadRequestException(
+            BadRequestException e,
+            WebRequest request
     ){
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(createErrorMap(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+                .body(createErrorMap(request.getDescription(false), e.getMessage()));
     }
 
     // Internal Server Error
     @ExceptionHandler(InternalServerError.class)
-    public ResponseEntity<Map<String, String>> handleInternalServerError(InternalServerError e){
+    public ResponseEntity<Map<String, Object>> handleInternalServerError(
+            InternalServerError e, WebRequest request
+    ) {
         return ResponseEntity.
                 status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(
-                        createErrorMap(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage())
+                        createErrorMap(request.getDescription(false), "Internal Server Error")
                 );
     }
 
     // Notfound Exceptions
     @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleNotFoundException(ChangeSetPersister.NotFoundException e){
+    public ResponseEntity<Map<String, Object>> handleNotFoundException(
+            ChangeSetPersister.NotFoundException e,
+            WebRequest request
+    ){
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(createErrorMap(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+                .body(createErrorMap(request.getDescription(false), e.getMessage()));
     }
 
     // Remaining Exceptions
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleAllExceptions(Exception e) {
+    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception e, WebRequest request) {
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(createErrorMap(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
+                .body(createErrorMap(request.getDescription(false), e.getMessage()));
+    }
+
+    // Not Readable Exception
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, Object>> handleNotReadablePropertyException(
+            HttpMessageNotReadableException e,
+            WebRequest request
+    ){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(createErrorMap(request.getDescription(false), e.getMessage()));
+    }
+
+    // Validation in controllers | NOT IN RECORD
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleConstraintViolationException(
+            ConstraintViolationException e, WebRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(createErrorMap(
+                        request.getDescription(false),
+                        e.getMessage()
+                ));
     }
 
     // Validation Errors
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
             MethodArgumentNotValidException e
     ){
-        Map<String, String> errors = new HashMap<>();
+        Map<String, Object> errors = new HashMap<>();
 
         e.getBindingResult()
                 .getFieldErrors()
@@ -77,12 +114,12 @@ public class GlobalExceptionHandler {
                 .body(errors);
     }
 
-    private Map<String, String> createErrorMap(int status, String message) {
-        Map<String, String> errors = new HashMap<>();
+    private Map<String, Object> createErrorMap(String path, String message) {
+        Map<String, Object> errors = new HashMap<>();
 
         errors.put("message", message);
-        errors.put("status", String.valueOf(status));
-        errors.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        errors.put("path", path);
+        errors.put("timestamp", LocalDateTime.now());
 
         return errors;
     }
